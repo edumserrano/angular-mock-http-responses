05cb14dd0404ad0ed429790cf79f45b78737a02b
Eduardo Serrano <eduardomserrano@outlook.com>
2024-01-11 18:19:02 +0000

    test error

--- code-demos/angular-proxy-bypass/src/app/app.component.ts
+++ code-demos/angular-proxy-bypass/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component2.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
